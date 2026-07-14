9e37d62fa4f15c1f14185a837767345f297b2a1d
River <foxnezz@gmail.com>
2026-07-14 13:09:46 +0300

    Initial Commit

---
+++ Minesolve/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Minesolve
+//
+//  Created by River Deem on 2026-07-14.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
